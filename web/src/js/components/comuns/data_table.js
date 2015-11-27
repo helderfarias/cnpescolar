07354c0759e108f70f7b38ce5fcd09de1ede34c2
helderfarias <helderfarias@gmail.com>
2015-11-27 16:15:01 -0300

    selecao de linhas incompleto

--- web/src/js/components/comuns/data_table.js
+++ web/src/js/components/comuns/data_table.js
@@ -4,9 +4,10 @@ import React from 'react';
 
 let DataTable = React.createClass({
 
-    propTypes: {
-       numberOfPages: React.PropTypes.number,
-       totalItems: React.PropTypes.number
+    getInitialState() {
+        return {
+            selectedRows: false
+        }
     },
 
     getDefaultProps() {
@@ -16,15 +17,32 @@ let DataTable = React.createClass({
         }
     },
 
+    selectAllRows(cb) {
+        if (cb) {
+            cb();
+        }
+
+        this.setState({ selectedRows: !this.state.selectedRows })
+    },
+
     createColumns() {
         if (this.isEmptyList(this.props.columns)) {
             console.error('columns is empty');
             return (<th></th>);
         }
 
-        let cols = this.props.columns.map((column, index) => {
-            return (<th key={index}>{column.title}</th>);
-        });
+        let cols = [];
+
+        cols.push((
+            <th key={0}>
+                <input type="checkbox" name="selectAll" value="#" onClick={this.selectAllRows}/>
+            </th>
+        ));
+
+        for (var index = 0; index < this.props.columns.length; index++) {
+            let column = this.props.columns[index];
+            cols.push((<th key={index + 1}>{column.title}</th>));
+        }
 
         return cols;
     },
@@ -40,11 +58,11 @@ let DataTable = React.createClass({
         }
 
         let rows = this.props.source.map((row, rowIndex) => {
-            let columns =  this.props.columns.map((column, columnIndex) => {
-                return (<td key={columnIndex}>{row[column.name]}</td>)
-            });
-
-            return (<tr key={rowIndex}>{columns}</tr>);
+            return (<TableRow key={rowIndex}
+                                value={row}
+                                columns={this.props.columns}
+                                initialSelected={this.state.selectedRows}
+                                onChageRow={this.chageRow}/>);
         });
 
         return rows;
@@ -60,7 +78,7 @@ let DataTable = React.createClass({
     render() {
         return (
             <div className="table-responsive">
-                <table className="table">
+                <table className="table table-condensed table-hover">
                     <thead>
                         <tr>{this.createColumns()}</tr>
                     </thead>
@@ -68,6 +86,55 @@ let DataTable = React.createClass({
                 </table>
             </div>
         );
+    }
+
+});
+
+let TableRow = React.createClass({
+
+    getInitialState() {
+        return {
+            selection: false
+        }
+    },
+
+    getDefaultProps() {
+        return {
+            value: {},
+            columns: []
+        }
+    },
+
+    componentDidMount() {
+        this.props.onSelectRow(this._onChange);
+    },
+
+    _onChange() {
+        console.log('ok');
+    },
+
+    selectRow(e) {
+        this.setState({ selection: !this.state.selection })
+    },
+
+    render() {
+        let checked = (this.props.initialSelected || this.state.selection);
+
+        let items = [];
+
+        items.push((
+            <td key={0} className="table-columns-selection">
+                <input type="checkbox" onClick={this.selectRow} checked={checked}/>
+            </td>
+        ));
+
+        for (let index = 0; index < this.props.columns.length; index++) {
+            let column = this.props.columns[index];
+
+            items.push((<td key={index + 1}>{this.props.value[column.name]}</td>));
+        }
+
+        return (<tr className={checked ? 'selected' : 'deselected'}>{items}</tr>);
     }
 
 });
